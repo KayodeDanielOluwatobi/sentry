f22c6282520452b9e36ec30d9fe3c65757fa2f57
Daniel Kayode <dkayode61@gmail.com>
2026-04-21 12:10:02 +0100

    fix: consolidate allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Allow connections from your mobile device IP to prevent NextJS overriding and blocking dev tools
-  allowedDevOrigins: ['192.168.170.194'],
-  allowedDevOrigins: ['192.168.151.214'],
+  allowedDevOrigins: ['192.168.170.194', '192.168.151.214'],
 };
 
 export default nextConfig;
